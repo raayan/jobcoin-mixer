c0c47a949f7f1b029c466be02f438e1088689ed9
Raayan Pillai <raayan@raayanpillai.com>
2019-08-26 21:45:33 -0400

    UI compiled and copied to static files

---
+++ src/main/resources/static/precache-manifest.837681e126298e40bb364f0fa2988467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        "revision": "2813ee90f74afb25e720fe21518780bd",
+        "url": "/index.html"
+    },
+    {
+        "revision": "24551c0a991afc2fd2a1",
+        "url": "/static/css/2.9692cc1a.chunk.css"
+    },
+    {
+        "revision": "f70c1a8f4a15b602ebed",
+        "url": "/static/css/main.2c1b4e4b.chunk.css"
+    },
+    {
+        "revision": "24551c0a991afc2fd2a1",
+        "url": "/static/js/2.e23163ed.chunk.js"
+    },
+    {
+        "revision": "f70c1a8f4a15b602ebed",
+        "url": "/static/js/main.7130e461.chunk.js"
+    },
+    {
+        "revision": "fe16211831b098d95012",
+        "url": "/static/js/runtime~main.71436dd8.js"
+    }
+]);
